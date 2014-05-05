af3eaafe3ddcfd1065ceaa10faef8025b33a1cad
Erik Welch <erik.n.welch@gmail.com>
2014-05-05 12:18:32 -0400

    Add test for `dev_skip_test`

--- cytoolz/tests/dev_skip_test.py
+++ cytoolz/tests/dev_skip_test.py
@@ -1,8 +1,8 @@
-import nose.tools
 import cytoolz
+from nose.tools import nottest, istest
 
 # Decorator used to skip tests for developmental versions of CyToolz
 if 'dev' in cytoolz.__version__:
-    dev_skip_test = nose.tools.nottest
+    dev_skip_test = nottest
 else:
-    dev_skip_test = nose.tools.istest
+    dev_skip_test = istest
