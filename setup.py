c25d1b322e2e54a819da84bc9154730d47770e4b
Phillip Cloud <cpcloud@gmail.com>
2014-12-21 12:44:30 -0500

    Use package_data to ship tests

--- setup.py
+++ setup.py
@@ -79,8 +79,8 @@ if __name__ == '__main__':
         maintainer='Erik Welch',
         maintainer_email='erik.n.welch@gmail.com',
         license = 'BSD',
-        packages=['cytoolz', 'cytoolz.tests'],
-        package_data={'cytoolz': ['*.pxd']},
+        packages=['cytoolz'],
+        package_data={'cytoolz': ['*.pxd', 'tests/*.py']},
         # include_package_data = True,
         keywords=('functional utility itertools functools iterator generator '
                   'curry memoize lazy streaming bigdata cython toolz cytoolz'),
